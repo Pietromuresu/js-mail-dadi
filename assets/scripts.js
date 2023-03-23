e32ea707dbe35bae23fee8c495f0cf679bd30bb7
Pietromuresu <muresupietro66@gmail.com>
2023-03-23 16:29:02 +0100

    add the mail list + ask user mail w/prompt

---
+++ assets/scripts.js
@@ -0,0 +1,24 @@
+
+
+const mailList = [
+  'ginopaoli@gmail.com', 
+  'barbaradurso@gmail.com',
+  'muresupietro66@gmail.com',
+  'lodovicoguelfi@gmail.com',
+  'federicolucia@gmail.com',
+  'fabriziocorona@gmail.com',
+  'fabriziotarducci@gmail.com',
+  'loremipsum@gmail.com',
+  'pierochiambretti@gmail.com',
+  'pupo@gmail.com',
+  'enzoiacchetti@gmail.com',
+  'ginostrada@gmail.com',
+  'lucianalitizzetto@gmail.com',
+  'giannisperti@gmail.com',
+  'mariadefilippi@gmail.com'
+]
+
+const userMail = prompt ('Inserisci la tua mail');
+console.log(userMail);
+
+
